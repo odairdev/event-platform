9b9eb26d774e9ddd902a46cc15fde61e4b3865b4
Odair José Custodio Junior <odair.dev@gmail.com>
2022-06-22 17:20:00 -0300

    Header and sidebar components implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,11 @@
-import { gql, useQuery } from "@apollo/client"
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`
-
-interface Lesson {
-  id: string;
-  title: string
-}
+import { Event } from './pages/Event'
 
 function App() {
 
-  const { data } = useQuery<{ lessons: Lesson[]}>(GET_LESSONS_QUERY)
 
   return (
     <div className="App">
-      <ul>
-        {data && data.lessons.map(lesson => (
-          <li>{lesson.title}</li>
-        ))}
-      </ul>
+      <Event />
     </div>
   )
 }
